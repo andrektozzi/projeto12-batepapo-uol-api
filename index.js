ae94f99a0f194862a658e34cbf543857c37b7820
andrektozzi <andrektozzi@gmail.com>
2022-06-21 12:20:48 -0300

    rota post participants adicionada

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import dayjs from 'dayjs';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const participants = [];
+const messages = [];
+
 app.post("/participants", (req, res) => {
-    res.send("OK");
+    const { name } = req.body
+
+    if(!name) {
+        res.sendStatus(422);
+        return;
+    }
+
+    if(participants.some((participant) => participant.name === name)) {
+        res.sendStatus(409);
+        return;
+    }
+
+    participants.push({ name, lastStatus: Date.now() });
+    messages.push({ from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format("HH:MM:SS") });
+
+    res.sendStatus(201);
 });
 
 app.get("/participants", (req, res) => {
