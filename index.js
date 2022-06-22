b76986a8a68b04edbd6ecf0bdf45d21f9f964358
andrektozzi <andrektozzi@gmail.com>
2022-06-22 09:55:23 -0300

    rota post participants com mongodb

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 import dayjs from 'dayjs';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const participants = [];
-const messages = [];
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+    db = mongoClient.db("bate-papo-uol");
+})
+
 
 app.post("/participants", (req, res) => {
     const { name } = req.body
 
-    if(!name) {
-        res.sendStatus(422);
-        return;
-    }
+    if(!name) return res.sendStatus(422);
 
-    if(participants.some((participant) => participant.name === name)) {
-        res.sendStatus(409);
-        return;
-    }
-
-    participants.push({ name, lastStatus: Date.now() });
-    messages.push({ from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format("HH:MM:SS") });
-
-    res.sendStatus(201);
+    const find = db.collection("participants").findOne({name: name});
+    find.then(participant => {
+        if(!participant){
+            const insert = db.collection("participants").insertOne({
+                name: name,
+                lastStatus: Date.now()
+            });
+            insert.then(() => {
+                const message = db.collection("messages").insertOne({
+                    from: name,
+                    to: 'Todos',
+                    text: 'entra na sala...',
+                    type: 'status',
+                    time: dayjs().format('HH:MM:SS')
+                });
+                message.then(() => res.sendStatus(201));
+            });
+        } else return res.sendStatus(409);
+    });
 });
 
 app.get("/participants", (req, res) => {
